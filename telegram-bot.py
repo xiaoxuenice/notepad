f288db2f7b558f4540c7082a381f23dd9137db34
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2023-10-25 16:25:25 +0800

    Create telegram-bot.py

---
+++ telegram-bot.py
@@ -0,0 +1,7 @@
+pip install python-telegram-bot
+import telegram
+bot=telegram.Bot(token="###:######J5PDnKs")
+bot.send_message(chat_id='@asdfe',text='okok no problem')
+
+关注@BotFather用户
+/newbot    	
